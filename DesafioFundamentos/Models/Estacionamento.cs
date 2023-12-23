24b0ce73cc2b4892a436739abd333179b2b2dc9a
Tamyres Silva <108297293+tamyrespereirasilva@users.noreply.github.com>
2023-12-22 23:03:50 -0300

    corrijindo erros

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -30,9 +30,9 @@ namespace DesafioFundamentos.Models
             if (veiculos.Any(x => x.ToUpper() == placa.ToUpper())){
                 
                 Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado: ");
-                Console.ReadLine();
 
-                int horas = 0;
+                int horas = int.Parse(Console.ReadLine());
+;
                 decimal valorTotal = precoInicial + precoPorHora * horas; 
 
                 veiculos.Remove(placa);
